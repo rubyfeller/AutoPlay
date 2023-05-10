897a9df82dd67f486d4b53c8d5dd9bf1ef698fa1
rubyfeller <ruby.feller@student.fontys.nl>
2023-05-10 14:46:20 +0200

    Implemented fetching and playing ANP-bulletin by reading XML (using Rome) through RSS-feed. Started implementing co-routines.

--- app/src/main/java/com/rubyf/autoplay/MainActivity.kt
+++ app/src/main/java/com/rubyf/autoplay/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.rubyf.autoplay
 
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.rometools.rome.io.SyndFeedInput
+import com.rometools.rome.io.XmlReader
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
-import com.spotify.protocol.types.Track
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,10 +28,52 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        playAudio()
+    }
+
+    private fun playAudio() {
+        lifecycleScope.launch {
+            delay(60000) // simple delay of 1 minute for the time being.. will be changed to hourly bulletin play
+            try {
+                val mp3Url = withContext(Dispatchers.IO) { getMp3UrlFromXml() }
+                val mediaPlayer = MediaPlayer()
+                mediaPlayer.setDataSource(mp3Url)
+                mediaPlayer.prepare()
+                mediaPlayer.start()
+                if (isPlaying) {
+                    spotifyAppRemote?.playerApi?.pause()
+                }
+                // release the MediaPlayer object when the audio playback is finished and restart Spotify
+                mediaPlayer.setOnCompletionListener {
+                    mediaPlayer.release()
+                    spotifyAppRemote?.playerApi?.resume()
+                }
+            } catch (e: Exception) {
+                Log.e("playAudio_catch", e.message, e)
+            }
+        }
+    }
+
+    private fun getMp3UrlFromXml(): String {
+        val url =
+            URL("https://www.omnycontent.com/d/playlist/56ccbbb7-0ff7-4482-9d99-a88800f49f6c/a49c87f6-d567-4189-8692-a8e2009eaf86/9fea2041-fccd-4fcf-8cec-a8e2009eeca2/podcast.rss")
+        val romeFeed = SyndFeedInput().build(XmlReader(url))
+        val entries = romeFeed.entries
+        // loop through the entries to find the MP3 URL
+        for (entry in entries) {
+            val enclosures = entry.enclosures
+            for (enclosure in enclosures) {
+                if (enclosure.type == "audio/mpeg") {
+                    return enclosure.url
+                }
+            }
+        }
+        throw Exception("MP3 URL not found")
     }
 
     override fun onStart() {
         super.onStart()
+
         val connectionParams = ConnectionParams.Builder(clientId)
             .setRedirectUri(redirectUri)
             .showAuthView(true)
@@ -46,13 +97,16 @@ class MainActivity : AppCompatActivity() {
     private fun connected() {
         spotifyAppRemote?.let {
             // Play a playlist
-            val playlistUri = "spotify:playlist:37i9dQZEVXbK4BFAukDzj3"
-            it.playerApi.play(playlistUri)
+//            val playlistUri = "spotify:playlist:37i9dQZEVXbK4BFAukDzj3"
+//            it.playerApi.play(playlistUri)
             // Subscribe to PlayerState
-            it.playerApi.subscribeToPlayerState().setEventCallback { playerState ->
-                val track: Track = playerState.track
-                Log.d("MainActivity", track.name + " by " + track.artist.name)
-            }
+//            it.playerApi.subscribeToPlayerState().setEventCallback { playerState ->
+//                val track: Track = playerState.track
+//                Log.d("MainActivity", track.name + " by " + track.artist.name)
+//            }
+
+            val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 50, 0)
 
             val skipPreviousImageView = findViewById<ImageView>(R.id.skipPrevious)
             val play = findViewById<ImageView>(R.id.Play)
